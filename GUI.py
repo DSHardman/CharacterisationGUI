1f5b4d1a4ee57ece19532761ac9868b72dda5a96
David <dshardman@outlook.com>
2022-02-15 16:40:57 +0000

    init

---
+++ GUI.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+positions = tk.StringVar(value=["Random", "Fixed", "x Line", "y Line", "From File"])
+position_list = tk.Listbox(listvariable=positions, selectmode="browse")
+
+depths = tk.StringVar(value=["Random", "Fixed", "From File"])
+depth_list = tk.Listbox(listvariable=depths, selectmode="browse")
+
+temps = tk.StringVar(value=["Uncontrolled", "Fixed", "From File"])
+temp_list = tk.Listbox(listvariable=temps, selectmode="browse")
+
+def my_function(listbox):
+    print(listbox.get(listbox.curselection()))
+
+
+position_list.bind('<<ListboxSelect>>', lambda event, a=position_list: my_function(a))
+
+# frame
+# label
+# button
+# checkbutton
+# radiobutton
+# entry
+# combobox or listbox
+
+position_list.pack()
+
+
+window.mainloop()
